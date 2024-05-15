144b571e470af62cca1c6c930c74d41bb8daac3b
Nguyễn Đình Phúc <86676921+aoreSynia@users.noreply.github.com>
2024-05-15 22:15:06 +0700

    15/5
    Update ai_service.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, session, render_template
 from google_auth_service.google_auth import google_auth_bp
+from ai_service.ai_service import ai_service_bp
 
 app = Flask(__name__)
 app.secret_key = "1"
@@ -11,6 +12,13 @@ app.register_blueprint(google_auth_bp, url_prefix="/")
 def index():
     # Redirect đến trang đăng nhập của Google
     return redirect(url_for("google_auth.homepage"))
+
+@app.route("/dashboard")
+def dashboard():
+    if 'user' not in session:
+        return redirect(url_for("google_auth.homepage"))
+    activities = session.get('activity', [])
+    return render_template('dashboard.html', activities=activities)
 
 if __name__ == "__main__":
     app.run(debug=True)
